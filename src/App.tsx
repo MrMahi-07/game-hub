ee48bcab39dda1017edb568d9a1b95d862bd3996
Mahesh <mrmahi353@gmail.com>
2023-04-02 00:05:28 +0530

    material ui tried

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import AlertButton from "./assets/components/AlertButton";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useState } from "react";
+import { Button, IconButton, Stack, TextField } from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import BasicAlerts from "./mui/BasicAlerts";
 
-const App = () => {
-	const text = "Lorem ipsum, dolor sit amet.";
+function App() {
+	const [darkMode, setDarkMode] = useState(false);
+
+	const darkTheme = createTheme({
+		palette: {
+			mode: darkMode ? "dark" : "light",
+		},
+	});
 
 	return (
-		<AlertButton alertColor="danger" button="Show Alert?">
-			{text}
-		</AlertButton>
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<IconButton
+				aria-label="delete"
+				onClick={() => setDarkMode(!darkMode)}
+			>
+				{darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+			</IconButton>
+			<BasicAlerts />
+			<Stack direction={"row"} spacing={2} margin={2}>
+				<TextField
+					id="outlined-basic"
+					label="Name"
+					variant="outlined"
+				/>
+				<Button variant="outlined">Outlined</Button>
+			</Stack>
+		</ThemeProvider>
 	);
-};
+}
 
 export default App;
---
+++ src/mui/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ButtonMui from "@mui/material/Button";
+
+const Button = () => {
+	return (
+		<div>
+			<ButtonMui>Hello World</ButtonMui>
+		</div>
+	);
+};
+
+export default Button;
